d3be8b6d462cdbfc167ea0e48c7a3e2b9207a214
olcayaltun <155670863+olcayaltun@users.noreply.github.com>
2024-08-23 22:12:06 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,18 @@ import PrepositionTest from "./PrepositionTest"; // Dosya adı PrepositionTest.j
 
 function App() {
   return (
-    <div className=" mt-4">
-      <h1 className=" text-white text-4xl grid place-items-center font-semibold mb-7 bg-slate-600 p-2  rounded-tl-[40px] rounded-br-[40px] max-md:w-[450px] ml-4">
+    <div className=" mt-4 grid place-items-center">
+      <h1 className=" text-white text-4xl grid place-items-center font-semibold mb-7 bg-slate-600 p-2  rounded-tl-[40px] rounded-br-[40px] max-md:w-[400px]  max-md:text-lg ">
         Verben mit Präpositionen
       </h1>
       <PrepositionTest />
+
+      <a href="https://www.instagram.com/lcy.ltn/">
+        {" "}
+        <h2 className="text-white border border-rose-500 mt-[-10px] p-1 rounded-lg hover:scale-95 hover:bg-rose-400">
+          @lcy.ltn
+        </h2>
+      </a>
     </div>
   );
 }
